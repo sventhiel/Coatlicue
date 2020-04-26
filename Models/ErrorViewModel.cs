6ff59b81614b3af62d611aa824fa4e749750de0c
Sven Thiel <sven.thiel@uni-jena.de>
2020-04-26 18:56:47 +0200

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Coatlicue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
